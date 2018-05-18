bf9b28ba2b4e48a9a7f5d9453ce69ec7a9f46a3a
Antonio Scandurra <as-cii@github.com>
2018-05-18 19:39:15 +0200

    WIP

--- xray_core/src/buffer.rs
+++ xray_core/src/buffer.rs
@@ -44,7 +44,6 @@ pub enum Error {
 }
 
 pub struct Buffer {
-    id: BufferId,
     pub replica_id: ReplicaId,
     next_replica_id: Option<ReplicaId>,
     local_clock: LocalTimestamp,
@@ -395,7 +394,6 @@ pub mod rpc {
         fn init(&mut self, _: &rpc::server::Connection) -> Self::State {
             let buffer = self.buffer.borrow_mut();
             let mut state = State {
-                id: buffer.id,
                 replica_id: self.replica_id,
                 fragments: Vec::new(),
                 insertions: HashMap::new(),
@@ -506,7 +504,7 @@ pub mod rpc {
 }
 
 impl Buffer {
-    pub fn new(id: BufferId) -> Self {
+    pub fn new() -> Self {
         let mut fragments = Tree::new();
 
         // Push start sentinel.
@@ -538,7 +536,6 @@ impl Buffer {
         );
 
         Self {
-            id,
             replica_id: 1,
             next_replica_id: Some(2),
             local_clock: 0,
@@ -598,7 +595,6 @@ impl Buffer {
         }
 
         let buffer = Buffer {
-            id: state.id,
             replica_id: state.replica_id,
             next_replica_id: None,
             local_clock: 0,
@@ -646,10 +642,6 @@ impl Buffer {
             .unwrap();
 
         Ok(buffer)
-    }
-
-    pub fn id(&self) -> BufferId {
-        self.id
     }
 
     pub fn next_replica_id(&mut self) -> Result<ReplicaId, ()> {
